28de4ad9117e9068ae8ab0d95d42dd0fc60475e8
NoBudgetStudios <144903504+NoBudgetStudios@users.noreply.github.com>
2023-09-13 18:10:57 +0300

    Delete src directory

--- src/scripts/contentFeeder.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Set the header text
-    document.getElementById("header").textContent = "Header";
-
-    // Set section descriptions
-    document.getElementById("section1-description").textContent = "Section1: section description";
-    document.getElementById("section2-description").textContent = "Section2: section description";
-});
